75a1ef2d6810b12f17fe6bd7cee02e77cbee4a43
Abdelrahman Abodief <abdelrahman.abodief.a@gmail.com>
2026-07-31 06:47:20 +0300

    wip: continue project development

---
+++ tests/api-clients/CancelOrderId.ts
@@ -0,0 +1,20 @@
+import { APIRequestContext } from '@playwright/test';
+import { endPoints } from '../test-data/testDataYamlReader';
+
+export class CancelOrderId {
+    private request: APIRequestContext;
+
+    constructor(request: APIRequestContext) {
+        this.request = request;
+    }
+
+    async cancelOrderId(orderId:string) {
+        const response = await this.request.get((endPoints.CancelOrderById`${orderId}`), {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            }
+        });
+
+        return response;
+    }
+}
---
+++ tests/api-clients/GetBookingByOrderId.ts
@@ -0,0 +1,25 @@
+import { APIRequestContext } from '@playwright/test';
+import { endPoints } from '../test-data/testDataYamlReader';
+
+export class GetBookingByOrderId {
+    private request: APIRequestContext;
+
+    constructor(request: APIRequestContext) {
+        this.request = request;
+    }
+
+    async getBookingByOrderId(orderId: string) {
+
+        const response = await this.request.post((endPoints.BookedItinerary), {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            form: {
+                order_id_text: orderId,
+                search_hotel_id: "Go"
+            }
+        });
+
+        return response;
+    }
+}
