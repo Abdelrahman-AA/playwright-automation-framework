44de98045d15f1f2f0e81ba14bf71a2cdebfcac7
Abdelrahman Abodief <abdelrahman.abodief.a@gmail.com>
2026-08-05 19:02:13 +0300

    wip: continue project development

---
+++ tests/specs/ForgotPassword.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from "../fixtures/fixtures";
+import { uiMSGs, validTestData } from "../test-data/testDataYamlReader";
+
+
+
+test.describe("Happy Path Suite", { tag: "@happy @Forgot-Password" }, () => {
+
+
+    test("Verify Sending Email To Reset Password When Entering Registered Email", async ({forgetPasswordPage}) => {
+        
+        await test.step("", async () => {
+            forgetPasswordPage.enterEmail(validTestData.)
+        });
+    });
+});
